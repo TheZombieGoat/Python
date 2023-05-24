ba9584c809f4575ae2f699b8d5d8bf3e9c9bc22e
TheZombieGoat <98498075+TheZombieGoat@users.noreply.github.com>
2023-05-24 17:23:06 -0400

    Create mad_py_lib.py
     Playing MadLib using a poem.

---
+++ Input-Output/mad_py_lib.py
@@ -0,0 +1,15 @@
+"""
+File: mad_py_lib.py
+Date: 02/09/2023
+"""
+
+place = input("What is a place name? ")
+noun1 = input("Give an example of a noun. ")
+noun2 = input("Give an example of another noun. ")
+familial_rs = input("Give an example of a familial relation. ")
+person_name = input("Give an example of a person's name. ")
+noun3 = input("Give yet another noun. ")
+
+print("There was a man from",place,"\nWho kept all his cash in a",noun2,"\n   B\
+ut his",familial_rs,",named",person_name,",\n   Ran away with a",noun3,"\nAnd a\
+s for the",noun2,",",place)
