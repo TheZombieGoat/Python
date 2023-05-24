619066e82d4bf9b8a2d77666311981b132869ae8
TheZombieGoat <98498075+TheZombieGoat@users.noreply.github.com>
2023-05-24 17:23:56 -0400

    Create best_things.py
    Asks about your favorite stuff and provides output

---
+++ Input-Output/best_things.py
@@ -0,0 +1,15 @@
+"""
+File : best_things.py
+Date : 02/09/2023
+"""
+
+fav_movie = input("Tell me your favorite movie: ")
+fav_song = input("Tell me your favorite song: ")
+fav_game = input("Tell me your favorite game: ")
+fav_food = input("Tell me your favorite food: ")
+
+print(fav_movie,"is a great film!")
+print("I love to eat",fav_food,"!")
+print("I heard great things about",fav_game,"!")
+print("Never heard of",fav_song,"though.")
+
