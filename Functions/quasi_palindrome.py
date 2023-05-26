9c1afd39d738904141d9a58bfedaba1ac4901fe0
TheZombieGoat <98498075+TheZombieGoat@users.noreply.github.com>
2023-05-25 22:14:36 -0400

    Cleaned code and renamed quasi_pal.py to quasi_palindrome.py

--- Functions/quasi_palindrome.py
+++ Functions/quasi_palindrome.py
@@ -6,17 +6,13 @@ Date:         03/10/2023
 EXIT_STRING = "quit"
 
 def quasi_palindrome(word, errors):
-    size = len(word)
-    half_size = int(len(word)/2)
-    if size % 2 == 1:
-        half_size += 1
-    list1 = list(word.lower())
     count = 0
-    j = size-1
-    for i in range(half_size):
-        if list1[i] != list1[j]:
+    i = 0
+    word = word.lower()
+    while i < len(word) // 2:
+        if word[i] != word[-(i+1)]:
             count += 1
-        j -= 1
+        i += 1
     if count <= errors:
         return True
     return False
